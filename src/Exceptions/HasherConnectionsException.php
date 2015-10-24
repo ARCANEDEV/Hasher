fe60b28b5d25a1a408bb831eb1c98f35b304a6ad
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-25 00:25:47 +0100

    Adding HasherConnectionsException

---
+++ src/Exceptions/HasherConnectionsException.php
@@ -0,0 +1,9 @@
+<?php namespace Arcanedev\Hasher\Exceptions;
+
+/**
+ * Class     HasherConnectionsException
+ *
+ * @package  Arcanedev\Hasher\Exceptions
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class HasherConnectionsException extends HasherException {}
