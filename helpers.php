b1247b89d1c6abb0a7903b262d74e33aace2a01d
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-25 15:25:03 +0000

    Updating the helper

--- helpers.php
+++ helpers.php
@@ -4,9 +4,9 @@ if ( ! function_exists('hasher')) {
     /**
      * Get the Hasher repository.
      *
-     * @return \Arcanedev\Hasher\Contracts\HashManager
+     * @return Arcanedev\Hasher\Contracts\HashManager
      */
     function hasher() {
-        return app('arcanedev.hasher');
+        return app(Arcanedev\Hasher\Contracts\HashManager::class);
     }
 }
