6d0b04aa002e0bd8da2c27d3417d505d83471355
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-09-15 16:17:30 +0100

    Updating the Contracts

---
+++ src/Contracts/HashDriver.php
@@ -0,0 +1,9 @@
+<?php namespace Arcanedev\Hasher\Contracts;
+
+/**
+ * Interface  HashDriver
+ *
+ * @package   Arcanedev\Hasher\Contracts
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface HashDriver extends Hashable {}
